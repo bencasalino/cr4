768eb33e03df69bf3796e74f99ecbefcfb5b064e
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-28 12:54:31 -0700

    add getters and setters for both classes

--- src/Brand.php
+++ src/Brand.php
@@ -7,5 +7,27 @@
         private $Brand_name;
     }
 
-    //contructor and getters/setters 
+    //contructor needed???????
+
+
+
+
+        //Getters and Setters
+        function getName()
+        {
+            return $this->name;
+        }
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+        function getId()
+        {
+            return $this->id;
+        }
+        ///////////////End Getters and Setters//////////////
+
+
+
+
 ?>
--- src/Store.php
+++ src/Store.php
@@ -11,5 +11,41 @@
           $this->id = $id;
       }
 
+     //Getters and Setters
+     function getName()
+     {
+         return $this->name;
+     }
+     function setName($new_name)
+     {
+         $this->name = $new_name;
+     }
+     function getLocation()
+     {
+         return $this->location;
+     }
+     function setLocation($new_location)
+     {
+         $this->location = $new_location;
+     }
+     function getId()
+     {
+         return $this->id;
+     }
+     ////////////////End Getters and Setters/////////////
+
+
+     needs save
+     update name
+     update loacation
+     add brand
+     get brands multiple
+
+
+     static functions
+     get all
+     dell all
+     find ? 
+
 
       ?>
