de4e494483c0e0f26ce9aea4d22bcc4cab47efc4
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-28 10:33:28 -0700

    start testing

---
+++ src/Store.php
@@ -0,0 +1,15 @@
+<?php
+      //Class
+    Class Store{
+      //Class Properties
+      private $id;
+      private $store_name;
+      //Constructors
+      function __construct($store_name, $id = null)
+      {
+          $this->store_name = $store_name;
+          $this->id = $id;
+      }
+
+
+      ?>
